1c4995c181b057001e6e41a827d072aa9410cf57
Jeremy Bennett <jeremy.bennett@gbmmec.com>
2015-11-30 17:00:07 +0000

    asdf

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -8,88 +8,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Define an error callback
-static void error_callback(int error, const char* description)
-{
-	fputs(description, stderr);
-	_fgetchar();
-}
-
-//Define the key input callback
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, GL_TRUE);
-}
-
 int main(void)
 {
-	//Set the error callback
-	glfwSetErrorCallback(error_callback);
-
-	//Initialize GLFW
 	if (!glfwInit())
 	{
-		exit(EXIT_FAILURE);
-	}
-
-	//Set the GLFW window creation hints - these are optional
-	//glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); //Request a specific OpenGL version
-	//glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //Request a specific OpenGL version
-	//glfwWindowHint(GLFW_SAMPLES, 4); //Request 4x antialiasing
-	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	//Declare a window object
-	GLFWwindow* window;
-
-	//Create a window and create its OpenGL context
-	window = glfwCreateWindow(640, 480, "Test Window", NULL, NULL);
-
-	//If the window couldn't be created
-	if (!window)
-	{
-		fprintf(stderr, "Failed to open GLFW window.\n");
-		glfwTerminate();
-		exit(EXIT_FAILURE);
-	}
-
-	//This function makes the context of the specified window current on the calling thread. 
-	glfwMakeContextCurrent(window);
-
-	//Sets the key callback
-	glfwSetKeyCallback(window, key_callback);
-
-	//Initialize GLEW
-	GLenum err = glewInit();
-
-	//If GLEW hasn't initialized
-	if (err != GLEW_OK)
-	{
-		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+		fprintf(stderr, "Failed to initialize GLFW \n");
 		return -1;
 	}
 
-	//Set a background color
-	glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	//Main Loop
-	do
-	{
-		//Clear color buffer
-		glClear(GL_COLOR_BUFFER_BIT);
+	GLFWwindow* window;
+	window = glfwCreateWindow(1024, 768, "Test window", NULL, NULL);
+	if (window == NULL) {
+		fprintf(stderr, "Failed to open GLFW window");
+		glfwTerminate();
+		return -1;
+	}
 
-		//Swap buffers
-		glfwSwapBuffers(window);
-		//Get and organize events, like keyboard and mouse input, window resizing, etc...
-		glfwPollEvents();
+	glfwMakeContextCurrent(window);
+	glewExperimental = true;
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize glew");
+		return -1;
+	}
 
-	} //Check if the ESC key had been pressed or if the window had been closed
-	while (!glfwWindowShouldClose(window));
-
-	//Close OpenGL window and terminate GLFW
-	glfwDestroyWindow(window);
-	//Finalize and clean up GLFW
-	glfwTerminate();
-
-	exit(EXIT_SUCCESS);
 }
