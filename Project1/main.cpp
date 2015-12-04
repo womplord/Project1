69926f2e046efe42a195023486fceb9c827d6ff8
Jeremy Bennett <jeremy.bennett@gbmmec.com>
2015-12-04 16:54:26 +0000

    adf

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -23,7 +23,7 @@ int main(void)
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLFWwindow* window;
-	window = glfwCreateWindow(1024, 768, "Test window", NULL, NULL);
+	window = glfwCreateWindow(400, 300, "Test window", NULL, NULL);
 	if (window == NULL) {
 		fprintf(stderr, "Failed to open GLFW window");
 		glfwTerminate();
@@ -37,4 +37,48 @@ int main(void)
 		return -1;
 	}
 
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	do {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
+
+	//set up VAO
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	//specify triangle vertices
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+	};
+
+	//identify vertex buffer
+	GLuint vertexbuffer;
+	//generate 1 buffer and give to vertexbuffer
+	glGenBuffers(1, &vertexbuffer);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	//give vertices to opengl
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+
+	//start to draw triangle
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glVertexAttribPointer(
+		0,
+		3,
+		GL_FLOAT,
+		GL_FALSE,
+		0,
+		(void*)0
+	);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDisableVertexAttribArray(0);
 }
