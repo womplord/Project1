699aa343e0d4465f133cc2b0c8b229f83bcaba65
Jeremy Bennett <jeremy.bennett@gbmmec.com>
2015-12-07 13:32:36 +0000

    got white triangle to draw

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -124,18 +124,6 @@ int main(void)
 		return -1;
 	}
 
-	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-
-	GLuint programID = LoadShaders("SimpleVertexShader.vertexshader.txt", "SimpleFragmentShader.fragmentshader.txt");
-	do {
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glUseProgram(programID);
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-
-	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
-
 	//set up VAO
 	GLuint VertexArrayID;
 	glGenVertexArrays(1, &VertexArrayID);
@@ -147,30 +135,53 @@ int main(void)
 		1.0f, -1.0f, 0.0f,
 		0.0f, 1.0f, 0.0f,
 	};
-
 	//identify vertex buffer
 	GLuint vertexbuffer;
 	//generate 1 buffer and give to vertexbuffer
 	glGenBuffers(1, &vertexbuffer);
-	
+
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 	//give vertices to opengl
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-	//start to draw triangle
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glVertexAttribPointer(
-		0,
-		3,
-		GL_FLOAT,
-		GL_FALSE,
-		0,
-		(void*)0
-	);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-	glDisableVertexAttribArray(0);
+	//GLuint programID = LoadShaders("SimpleVertexShader.vertexshader.txt", "SimpleFragmentShader.fragmentshader.txt");
+	do {
+		//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		//glUseProgram(programID);
+
+
+
+		//start to draw triangle
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,
+			3,
+			GL_FLOAT,
+			GL_FALSE,
+			0,
+			(void*)0
+			);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDisableVertexAttribArray(0);
+
+
+
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
+
+
+
+
+
+
+
 
 
 }
