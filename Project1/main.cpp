3accd0cd226a718be8467df99aa261acfd2b4d43
Jeremy Bennett <jeremy.bennett@gbmmec.com>
2015-12-11 16:54:28 +0000

    worked a bit on tutorial 5

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -29,6 +29,46 @@ GLuint loadBMP_custom(const char * imagepath)
 	unsigned int width, height;
 	unsigned int imageSize;
 	unsigned char* data;
+
+	FILE * file = fopen(imagepath, "rb");
+	if (!file) { printf("Image could not be opened\n"); return 0; }
+
+	//read 54 byte header
+	if (fread(header, 1, 54, file) != 54) {
+		printf("Not a correct BMP file \n");
+		return false;
+	}
+
+	if (header[0] != 'B' || header[1] != 'M') {
+		printf("Error: No BM in file header \n");
+	}
+
+	dataPos = *(int*)&(header[0x0A]);
+	imageSize = *(int*)&(header[0x22]);
+	width = *(int*)&(header[0x12]);
+	height = *(int*)&(header[0x16]);
+
+	//handle incorrectly formatted BMP
+	if (imageSize == 0) imageSize = width*height * 3;
+	if (dataPos == 0) dataPos = 54;
+
+	//get data
+	data = new unsigned char[imageSize];
+	fread(data, 1, imageSize, file);
+
+	fclose(file);
+
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+
+	//bind texture
+	glBindTexture(GL_TEXTURE_2D, textureID);
+
+	//give image to opengl
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, data);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 }
 
 int main(void)
@@ -62,6 +102,7 @@ int main(void)
 		fprintf(stderr, "Failed to initialize glew");
 		return -1;
 	}
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
 	//set up VAO
 	GLuint VertexArrayID;
@@ -170,7 +211,7 @@ int main(void)
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
 
 
-	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+	
 
 	GLuint programID = LoadShaders("SimpleVertexShader.vertexshader.txt", "SimpleFragmentShader.fragmentshader.txt");
 
